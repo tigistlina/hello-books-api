55744833084d593ea08c73ec0bc87b48b84f805d
Tigist Woldearegai <tigistlina@gmail.com>
2023-04-20 01:16:15 -0500

    defining more end point

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+
+hello_world_bp = Blueprint("hello_world", __name__)
+# hello_world_bp = Blueprint("hello/JSON", __name__)
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    my_beautiful_response_body = "Hello, World!"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
