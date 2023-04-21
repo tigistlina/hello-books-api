b5fe3429f17e05e8ebcb1170edd2c1799846fd0d
Tigist Woldearegai <tigistlina@gmail.com>
2023-04-20 23:39:23 -0500

    Add response to handle 404

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
 
 hello_world_bp = Blueprint("hello_world", __name__)
-# hello_world_bp = Blueprint("hello/JSON", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response), 200
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title":book.title,
+                "description": book.description
+            }
+    return {"message": f"book {book_id} not found"} , 404
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
@@ -25,4 +60,4 @@ def broken_endpoint():
     }
     new_hobby = "Surfing"
     response_body["hobbies"].append(new_hobby)
-    return response_body
+    return response_bod
