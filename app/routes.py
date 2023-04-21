2e4e720e3c859f7ccb203af65249c1f9e2ef6e25
Tigist Woldearegai <tigistlina@gmail.com>
2023-04-20 23:44:36 -0500

    Add response to handle 400 in books/<book_id>

--- app/routes.py
+++ app/routes.py
@@ -19,16 +19,21 @@ books_bp = Blueprint("books", __name__, url_prefix="/books")
 def handle_books():
     books_response = []
     for book in books:
-        books_response.append({
+        books_response.append(
+            {
             "id": book.id,
             "title": book.title,
             "description": book.description
-        })
+        }
+                        )
     return jsonify(books_response), 200
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    book_id = int(book_id)
+    try:
+        book_id = int(book_id)
+    except:
+        return {"message": f"book {book_id} invalid"} , 400
     for book in books:
         if book.id == book_id:
             return {
