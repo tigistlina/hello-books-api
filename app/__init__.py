22958f184bda90c85648c16c89118c996602b218
Tigist Woldearegai <tigistlina@gmail.com>
2023-04-27 01:21:49 -0500

    makes models class and creates table

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
+#postgresql+psycopg2://postgres:postgres@localhost:5432/REPLACE_THIS_LAST_PART_WITH_DB_NAME
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    # DB config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from app.models.book import Book
     from .routes import hello_world_bp , books_bp
     app.register_blueprint(hello_world_bp)
     app.register_blueprint(books_bp)
