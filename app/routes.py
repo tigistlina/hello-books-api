8a43b77c120bb570f8eab8efbdcad973485c2674
Tigist Woldearegai <tigistlina@gmail.com>
2023-05-01 00:19:32 -0500

    adds query param key word arguments on read all books route

--- app/routes.py
+++ app/routes.py
@@ -20,8 +20,14 @@ def validate_book(book_id):
 # route functions
 @books_bp.route("",methods=["GET"])
 def read_all_books():
-    books = Book.query.all()
     books_response = []
+    title_query = request.args.get("title")
+
+    if title_query is not None:
+        books = Book.query.filter_by(title=title_query)
+    else:
+        books = Book.query.all()
+        
     for book in books:
         books_response.append(
             {
